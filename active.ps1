751cc35be971ead9fbbd917bb05d8e9c376c3591
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2024-09-20 20:25:04 -0400

    Update active.ps1

--- active.ps1
+++ active.ps1
@@ -8,48 +8,27 @@ function Ask-Mack {
     if ($NewLine) { Write-Host "" }
 }
 
-function Get-WinVer {
-    switch ((Get-CimInstance -ClassName Win32_OperatingSystem).OperatingSystemSKU) {
-        48 { "Pro" }
-        49 { "Pro N" }
-        96 { "Edu" }
-        97 { "Edu N" }
-        98 { "Ent 2015 LTSB" }
-        99 { "Ent 2015 LTSB N" }
-        121 { "Pro Wks" }
-        122 { "Pro Wks N" }
-        125 { "Pro Edu" }
-        126 { "Pro Edu N" }
-        4  { "Ent" }
-        Default { "Unsupported Version" }
-    }
-}
-
 function Activate-Win {
     Ask-Mack "Ask Mack for this : " -NewLine
     $hstName = Read-Host "Enter KMS Hostname"
     
-    $commands = @("-upk", "-ipk W269N-WFGWX-YVC9B-4J6C9-T83GX", "-skms $hstName", "-ato")
+    $commands = @("-upk", "-ipk W269N-WFGWX-YVC9B-4J6C9-T83GX", "-skms $hstName", "-ato", "-dlv")
     foreach ($cmd in $commands) {
         Start-Process "slmgr.vbs" -ArgumentList $cmd -Wait
         switch ($cmd.split()[0]) {
-            "-upk" { Write-Host "Uninstall Key" -ForegroundColor Yellow }
+            "-upk" { Write-Host "Uninstalling Old Key" -ForegroundColor Yellow }
             "-ipk" { Write-Host "Install New Key" -ForegroundColor Yellow }
-            "-skms" { Write-Host "Server Set" -ForegroundColor Yellow }
-            "-ato" { Write-Host "Activation Attempted" -ForegroundColor Yellow }
+            "-skms" { Write-Host "Setting Server" -ForegroundColor Yellow }
+            "-ato" { Write-Host "Activating" -ForegroundColor Yellow }
+            "-dlv" { Write-Host "Displaying Info" -ForegroundColor Yellow }
         }
-        Start-Sleep 5
+        Start-Sleep 10
     }
-
-    # Check if the current Windows version matches the valid activated ones
-    $sku = (Get-CimInstance -ClassName Win32_OperatingSystem).OperatingSystemSKU
-    if ($sku -in 4, 48, 96, 98, 121, 125) {
-        Write-Host "Windows is activated and the version is supported." -ForegroundColor Green
-        return $true
-    } else {
-        Write-Host "Windows activation failed or unsupported version." -ForegroundColor Red
-        return $false
-    }
+    $os = Get-CimInstance -ClassName Win32_OperatingSystem
+    Write-Host $("New Version: " + ($os = Get-CimInstance -ClassName Win32_OperatingSystem).caption) -ForegroundColor Yellow
+    if ($os.caption -like "*Pro*" -or $osEdition -like "*Enterprise*" -or $osEdition -like "*Education*") {
+        Write-Host "Windows is activated and the version is supported." -ForegroundColor Green; return $true} 
+    else { Write-Host "Windows activation failed or unsupported version." -ForegroundColor Red; return $false}
 }
 
 function Enable-BLC {
@@ -66,17 +45,13 @@ function Enable-BLC {
 
 function Check-BLStat {
     $status = Get-BitLockerVolume -MountPoint "C:"
-    if ($status.ProtectionStatus -eq 'On') {
-        Write-Host "BitLocker is enabled on C:. Encryption: $($status.EncryptionPercentage)%." -ForegroundColor Green
-    } else {
-        Write-Host "BitLocker is not enabled on C:." -ForegroundColor Red
-    }
+    if ($status.ProtectionStatus -eq 'On') { Write-Host "BitLocker is enabled on C:. Encryption: $($status.EncryptionPercentage)%." -ForegroundColor Green} 
+    else {Write-Host "BitLocker is not enabled on C:." -ForegroundColor Red}
 }
 
 # Main Logic
 Write-Host "Current Key: $((Get-WmiObject -Query 'Select OA3xOriginalProductKey from SoftwareLicensingService').OA3xOriginalProductKey)" -ForegroundColor Yellow
-Write-Host "Current Version: " -NoNewline -ForegroundColor Yellow
-Write-Host $(Get-WinVer) -ForegroundColor Yellow
+Write-Host $("Current Version: " + ($os = Get-CimInstance -ClassName Win32_OperatingSystem).caption) -ForegroundColor Yellow
 
 if (Activate-Win) {
     Ask-Mack "This will now encrypt drive. Are you sure? : " -NewLine
